3edc897d2e30cdd1c3a924f0bc9d92f72d05e2a4
tomoki013 <tomoki_ttttt@icloud.com>
2025-01-16 13:56:53 +0900

    ページネーションボタンの作成、機能は未実装

--- app/components/elements/pagination/Pagination.tsx
+++ app/components/elements/pagination/Pagination.tsx
@@ -1,6 +1,18 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
 const Pagination: React.FC = () => {
+    const pathname = usePathname();
+
+    if (!pathname.includes('/blogList') && !pathname.includes('/travelinfo')) {
+        return null;
+    }
+
     return (
-        <h1>a</h1>
+        <ul className="flex justify-center">
+            <li className={`border border-[var(--color-one)] text-[var(--color-one)] rounded p-1 m-1 hover:text-white hover:bg-[var(--color-one)]`}>1</li>
+        </ul>
     );
 }
 
